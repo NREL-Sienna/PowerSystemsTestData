1519f47c58ceceb9b5088b57c4f261c772e9ab73
Jose Daniel Lara <jdlara@berkeley.edu>
2022-08-09 20:48:40 -0700

    order files

---
+++ psid_tests/data_tests/test13.jl
@@ -0,0 +1,54 @@
+using PowerSystems
+using NLsolve
+const PSY = PowerSystems
+
+############### Data Network ########################
+include(joinpath(dirname(@__FILE__), "dynamic_test_data.jl"))
+include(joinpath(dirname(@__FILE__), "data_utils.jl"))
+############### Data Network ########################
+threebus_file_dir = joinpath(dirname(@__FILE__), "ThreeBusNetwork.raw")
+threebus_sys = System(threebus_file_dir, runchecks = false)
+add_source_to_ref(threebus_sys)
+
+function dyn_gen_avr_type2(generator)
+    return PSY.DynamicGenerator(
+        name = get_name(generator), #static generator
+        ω_ref = 1.0, # ω_ref
+        machine = machine_oneDoneQ(), #machine
+        shaft = shaft_no_damping(), #shaft
+        avr = avr_type2(), #avr
+        prime_mover = tg_type1(), #tg
+        pss = pss_none(),
+    ) #pss
+end
+
+function dyn_gen_simple_avr(generator)
+    return PSY.DynamicGenerator(
+        name = get_name(generator), #static generator
+        ω_ref = 1.0, # ω_ref
+        machine = machine_oneDoneQ(), #machine
+        shaft = shaft_no_damping(), #shaft
+        avr = avr_propr(), #avr
+        prime_mover = tg_none(), #tg
+        pss = pss_none(),
+    ) #pss
+end
+
+for l in get_components(PSY.PowerLoad, threebus_sys)
+    PSY.set_model!(l, PSY.LoadModels.ConstantImpedance)
+end
+
+# Add dynamic generators to the system (each gen is linked through a static one)
+for g in get_components(Generator, threebus_sys)
+    if get_number(get_bus(g)) == 102
+        case_gen = dyn_gen_avr_type2(g)
+        add_component!(threebus_sys, case_gen, g)
+    elseif get_number(get_bus(g)) == 103
+        case_gen = dyn_gen_simple_avr(g)
+        add_component!(threebus_sys, case_gen, g)
+    end
+end
+
+#Compute Y_bus after fault
+fault_branches = deepcopy(collect(get_components(Branch, threebus_sys))[2:end])
+Ybus_fault = PSY.Ybus(fault_branches, get_components(Bus, threebus_sys))[:, :]
---
+++ psid_tests/data_tests/test17.jl
@@ -0,0 +1,35 @@
+using PowerSystems
+using NLsolve
+const PSY = PowerSystems
+
+include(joinpath(dirname(@__FILE__), "dynamic_test_data.jl"))
+include(joinpath(dirname(@__FILE__), "data_utils.jl"))
+############### Data Network ########################
+
+raw_file_dir = joinpath(dirname(@__FILE__), "../benchmarks/psse/GENROU/ThreeBusMulti.raw")
+sys = System(raw_file_dir);
+
+#Replace Gen101 by Source
+remove_component!(ThermalStandard, sys, "generator-101-1");
+add_source_to_ref(sys)
+
+function dyn_gen_genrou(generator)
+    return PSY.DynamicGenerator(
+        name = get_name(generator),
+        ω_ref = 1.0, #ω_ref
+        machine = machine_genrou(), #machine
+        shaft = shaft_genrou(), #shaft
+        avr = avr_type1(), #avr
+        prime_mover = tg_none(), #tg
+        pss = pss_none(),
+    ) #pss
+end
+
+for l in get_components(PSY.PowerLoad, sys)
+    PSY.set_model!(l, PSY.LoadModels.ConstantImpedance)
+end
+
+#Add GENROU to System
+g = get_component(ThermalStandard, sys, "generator-102-1")
+dyn_gen = dyn_gen_genrou(g)
+add_component!(sys, dyn_gen, g);
