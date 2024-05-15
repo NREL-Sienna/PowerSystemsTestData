d6b8f07429b2abdde1a6410a2a8d13a9aec21ab2
Jose Daniel Lara <jdlara@berkeley.edu>
2024-05-15 12:59:34 -0600

    update storage

--- psy_data/data_5bus_pu.jl
+++ psy_data/data_5bus_pu.jl
@@ -761,9 +761,10 @@ phes5(nodes5) = [
     ),
 ];
 
-battery5(nodes5) = [GenericBattery(
+battery5(nodes5) = [EnergyReservoirStorage(
     name = "Bat",
     prime_mover_type = PrimeMovers.BA,
+    storage_technology_type = StorageTech.OTHER_CHEM,
     available = true,
     bus = nodes5[1],
     initial_energy = 2.0,
