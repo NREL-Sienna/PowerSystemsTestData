49c1db7f56b9bf39d1b0971b6c6402fa1ff57cf7
Mia Moore <198999385+mmoore-MEM@users.noreply.github.com>
2025-06-24 15:33:08 -0600

    ACBus(; kwargs) function changed, so updated commands

--- psy_data/data_5bus_pu.jl
+++ psy_data/data_5bus_pu.jl
@@ -20,11 +20,11 @@ function nodes5()
     #ACBus(3, "nodeC", "PV", 0, 1.0, (min = 0.9, max = 1.05), 230, nothing, nothing),
     #ACBus(4, "nodeD", "REF", 0, 1.0, (min = 0.9, max = 1.05), 230, nothing, nothing),
     #ACBus(5, "nodeE", "PV", 0, 1.0, (min = 0.9, max = 1.05), 230, nothing, nothing),
-    ACBus(number = 1, name= "nodeA", available = true, bustype = "PV", angle = 0.0, magnitude = 1.0, voltage_limits = (min = 0.9, max = 1.05), base_voltage = 230,),
-    ACBus(number = 2, name= "nodeB", available = true, bustype = "PQ", angle = 0.0, magnitude = 1.0, voltage_limits = (min = 0.9, max = 1.05), base_voltage = 230,),
-    ACBus(number = 3, name= "nodeC", available = true, bustype = "PV", angle = 0.0, magnitude = 1.0, voltage_limits = (min = 0.9, max = 1.05), base_voltage = 230,),
-    ACBus(number = 4, name= "nodeD", available = true, bustype = "REF", angle = 0.0, magnitude = 1.0, voltage_limits = (min = 0.9, max = 1.05), base_voltage = 230,),
-    ACBus(number = 5, name= "nodeE", available = true, bustype = "PV", angle = 0.0, magnitude = 1.0, voltage_limits = (min = 0.9, max = 1.05), base_voltage = 230,),
+    ACBus(number = 1, name= "nodeA", bustype = "PV", angle = 0.0, magnitude = 1.0, voltage_limits = (min = 0.9, max = 1.05), base_voltage = 230,),
+    ACBus(number = 2, name= "nodeB",  bustype = "PQ", angle = 0.0, magnitude = 1.0, voltage_limits = (min = 0.9, max = 1.05), base_voltage = 230,),
+    ACBus(number = 3, name= "nodeC",  bustype = "PV", angle = 0.0, magnitude = 1.0, voltage_limits = (min = 0.9, max = 1.05), base_voltage = 230,),
+    ACBus(number = 4, name= "nodeD",  bustype = "REF", angle = 0.0, magnitude = 1.0, voltage_limits = (min = 0.9, max = 1.05), base_voltage = 230,),
+    ACBus(number = 5, name= "nodeE",  bustype = "PV", angle = 0.0, magnitude = 1.0, voltage_limits = (min = 0.9, max = 1.05), base_voltage = 230,),
     ];
 return nodes
 end
